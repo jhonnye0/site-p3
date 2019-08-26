852c9ef464118de78e4ae799db5c18362fd2c910
Jhonnye <43444151+jhonnye0@users.noreply.github.com>
2019-08-26 11:09:26 -0300

    Add files via upload

--- site/frontend/src/routes.js
+++ site/frontend/src/routes.js
@@ -4,28 +4,31 @@ import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import Main from './pages/Main';
 import Register from './pages/Register';
-// import { isAuthenticated } from './Auth'
+import Contract from './pages/Contract';
+import Edit from './pages/Edit';
 
-// const PrivateRoute = ({ component: Component, ...rest }) => (
-//     <Route 
-//         {...rest} 
-//         render={props => 
-//             isAuthenticated() ? (
-//                 (<Component {...props} />)
-//             ) : (
-//                 <Redirect to={{pathname: '/', state: {from: props.location} }}/>
-//             )
-//         } 
-//     />
-// );
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route 
+        {...rest} 
+        render={ props => 
+            true ? (
+                (<Component {...props} />)
+            ) : (
+                <Redirect to={{ pathname: '/', state: { from: props.location } }}/>
+            )
+        } 
+    />
+);
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Login} />
-                <Route path="/main" component={Main} />
+                <PrivateRoute path="/user/:userId" component={Main} />
                 <Route path="/register" component={Register} />
+                <Route path="/contract/:userId/:id" component={Contract} />
+                <Route path="/edit/user/:userId" component={Edit} />
             </Switch> 
         </BrowserRouter>
     );
