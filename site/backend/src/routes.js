e26ebb117a293a019519bff6ad5ad7defd58f4e7
Jhonnye <43444151+jhonnye0@users.noreply.github.com>
2019-08-26 11:07:42 -0300

    Add files via upload

--- site/backend/src/routes.js
+++ site/backend/src/routes.js
@@ -6,9 +6,27 @@ const UserController = require('./controllers/UserController');
 const ContractController = require('./controllers/ContractController');
 const LoginController = require('./controllers/LoginController');
 
-routes.post('/user', UserController.store);
-routes.post('/user/assassins/:userId', LikeController.store);
-routes.post('/user/assassins/:userId/contract', ContractController.store);
-routes.post('/login', LoginController.LogInAuthentication)
+routes.get(`/user/:id`, UserController.index);
+routes.post('/user', (req, res) => {
+    UserController.store(req,res);
+});
+
+routes.post('/login', (req, res) => {
+    LoginController.LogInAuthentication(req,res);
+});
+
+routes.post('/user/assassins/:userId/likes', (req, res) => {
+    LoginController.LogInAuthenticationControl(req, res, LikeController);
+});
+
+routes.post('/login/user', (req, res) => {
+    LoginController.LogInAuthenticationControl(req, res, null);
+});
+
+routes.post('/user/assassins/:id/contract', (req, res) => {
+    LoginController.LogInAuthenticationControl(req, res, ContractController);
+});
+
+routes.post('/user/:userId/edit', UserController.update);
 
 module.exports = routes;
--- site/backend/src/server.js
+++ site/backend/src/server.js
@@ -1,23 +1,19 @@
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
-
+const cors = require('cors');
 const routes = require('./routes')
 
 mongoose.connect('mongodb+srv://admin:admin@serverp3-mydtc.mongodb.net/serverP3?retryWrites=true&w=majority',{
   useNewUrlParser : true
 });
 
-server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader('Access-Control-Allow-Methods', ['PATCH', 'POST', 'GET', 'DELETE', 'PUT']);
-  res.setHeader('Access-Control-Allow-Headers', ['Content-Type']);
-  res.setHeader('Access-Control-Expose-Headers', ['Content-Type']);
-  next();
-});
+server.use(cors());
+server.options('*', cors());
 
 server.use(express.json());
 server.use(routes);
 
-server.listen(3333);
+var port = process.env.PORT || 3333;
+
+server.listen(port);
